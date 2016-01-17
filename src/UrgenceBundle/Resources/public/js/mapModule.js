748c08f3a69c097c1edf3dd40b09fb5ddbe00236
unknown <camille.dourferpignon@gmail.com>
2016-01-17 12:27:30 +0100

    Commit bloc infos page visualisation des alertes.

--- src/UrgenceBundle/Resources/public/js/mapModule.js
+++ src/UrgenceBundle/Resources/public/js/mapModule.js
@@ -1,6 +1,8 @@
 var mapModule = (function() {
     
     var markers = [];
+    var markersData = [];
+    var cards = [];
     
     function init(map_id) {
         var map = new google.maps.Map(document.getElementById(map_id), {
@@ -12,6 +14,7 @@ var mapModule = (function() {
     
     function loadData(map) {
        var array = [];
+       var arrayData = [];
        $.getJSON(BASE_URL + '/all-alerts', function(data) {
            var items = data.features;
            $.each(items, function(index, value) {
@@ -24,12 +27,30 @@ var mapModule = (function() {
                var properties = value["properties"];
                data["title"] = properties["title"];
                data["description"] = properties["description"];
+               if (properties["other-data"] !== null && properties["other-data"] != undefined) {
+                   data["nom"] = properties["other-data"]["lastname"];
+                   data["prenom"] = properties["other-data"]["firstname"];
+                   data["heure"] = properties["other-data"]["timestamp_alerte"];
+                   data["age"] = properties["other-data"]["age"];
+                   data["sexe"] = properties["other-data"]["genre"];
+                   data["severity"] = properties["other-data"]["severity"];
+               }
+               switch(data["severity"]) {
+                   case "Critique": data["color"] = "black";
+                                    break;
+                   case "Majeure": data["color"] = "red";
+                                    break;
+                   case "Mineure" : data["color"] = "yellow";
+                                    break;
+                   default: data["color"] = "green";
+               }
                if (!containsMarker(data.title)) {
                     addMarker(map, data, array);
+                    arrayData.push(data);
                }
            });
            markers = markers.concat(array);
-           console.log(markers.length);
+           setInfoPanel(arrayData);
        });
     };
     
@@ -64,11 +85,13 @@ var mapModule = (function() {
     function getInfoWindowContent(data) {
         return "<table style='width=100%;'>"
             + "<tr>"
-            + "<td>"+ data.title +"</td>"
+            + "<td><strong>"+ data.prenom + " " + data.nom +"</strong></td>"
             + "</tr>"
             + "<tr>"
-            + "<td>"+ data.description +"</td>"
+            + "<td>Lancée à "+ data.heure +"</td>"
             + "</tr>"
+            + "<tr>"
+            + "<td>Type : " + data.severity + "</td>"
             + "</table>"
     };
     
@@ -77,6 +100,47 @@ var mapModule = (function() {
         markers[i]["obj"].setMap(null);
       }
       markers = [];
+    };
+    
+    function setInfoPanel(arrayData) {
+        $.each(arrayData, function(index, data) {
+            $(".cards-container").append(getCard(index, data));
+            $('#card-'+index).paperCollapse();
+        });
+    };
+    
+    function getCard(index, properties) {
+        return '<div id="card-'+index+'" class="collapse-card '+properties.color+'">'
+             + '<div class="title"> <strong><i class="fa fa-user"></i> - '+ properties.prenom + ' ' + properties.nom +'</strong> </div>'
+             +  '<div class="body">'
+             +  '<form role="form">'
+             +  '<div class="form-group">'
+             +  '<label for="nom">Nom : </label>'
+             +  '<p id="nom" class="form-control-static">'+properties.nom+'</p>'
+             +  '</div>'
+             +  '<div class="form-group">'
+             +      '<label for="prenom">Prénom : </label>'
+             +      '<p id="prenom" class="form-control-static">'+properties.prenom+'</p>'
+             +   '</div>'
+             + '<div class="form-group">'
+             +     '<label for="age">Âge : </label>'
+             +     '<p id="age" class="form-control-static">'+properties.age+'</p>'
+             + '</div>'
+             + '<div class="form-group">'
+             +     '<label for="sexe">Sexe : </label>'
+             +     '<p id="sexe" class="form-control-static">'+properties.sexe+'</p>'
+             + '</div>'
+             + '<div class="form-group">'
+             + '<label for="pos">Localisation : </label>'
+             + '<p id="pos" class="form-control-static">('+properties.lat+', '+properties.lng+')</p>'
+             + '</div>'
+             + '<div class="form-group">'
+             + '<label for="heure">Heure de l\'alerte : </label>'
+             + '<p id="heure" class="form-control-static">'+new Date(properties.heure).toLocaleTimeString()+'</p>'
+             + '</div>'
+             + '</form>'
+             + '</div>'
+             + '</div>';
     };
     
     return {
